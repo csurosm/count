c47fd379a9a5f04f238200b3c482e168cbbafe71
Miklós Csűrös <csurosm@gmail.com>
2023-08-03 09:35:50 -0400

    Add count/von/*.java

---
+++ src/count/von/Count.java
@@ -0,0 +1,17 @@
+package count.von;
+
+/**
+ * Doppelgänger. 
+ * 
+ * @author csuros
+ *
+ */
+public class Count 
+{
+	private Count(/*\ ._. \*/) {/* | */};
+	
+	public static void main(String[] args) throws Exception
+	{
+		count.Count.main(args);
+	}
+}
